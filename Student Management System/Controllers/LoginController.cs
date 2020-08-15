2d6f39b61596bf0a225b7e5740cac97b2cdd3974
ashna100 <ashna.khanna100@gmail.com>
2020-08-16 02:48:23 +0530

    Student Management System

---
+++ Student Management System/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Student_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Management_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        StudentMSEntities db = new StudentMSEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(User obj1)
+        {
+            if (ModelState.IsValid)
+            {
+                using (StudentMSEntities db = new StudentMSEntities())
+                {
+                    var obj = db.Users.Where(a => a.Username.Equals(obj1.Username) && a.Password.Equals(obj1.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserId"] = obj.Id.ToString();
+                        Session["Username"] = obj.Username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The username or password is incorrect");
+                    }
+                }
+            }
+
+            return View(obj1);
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ Student Management System/Controllers/RegistrationController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Student_Management_System.Models;
+
+namespace Student_Management_System.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private StudentMSEntities db = new StudentMSEntities();
+
+        // GET: Registration
+        public ActionResult Index()
+        {
+            var registrations = db.Registrations.Include(r => r.Batch).Include(r => r.Course);
+            return View(registrations.ToList());
+        }
+
+        // GET: Registration/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // GET: Registration/Create
+        public ActionResult Create()
+        {
+            ViewBag.Batch_Id = new SelectList(db.Batches, "Id", "Batch1");
+            ViewBag.Course_Id = new SelectList(db.Courses, "Id", "Course1");
+            return View();
+        }
+
+        // POST: Registration/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Firstname,Lastname,Nic,Course_Id,Batch_Id,Telno")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Registrations.Add(registration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Batch_Id = new SelectList(db.Batches, "Id", "Batch1", registration.Batch_Id);
+            ViewBag.Course_Id = new SelectList(db.Courses, "Id", "Course1", registration.Course_Id);
+            return View(registration);
+        }
+
+        // GET: Registration/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Batch_Id = new SelectList(db.Batches, "Id", "Batch1", registration.Batch_Id);
+            ViewBag.Course_Id = new SelectList(db.Courses, "Id", "Course1", registration.Course_Id);
+            return View(registration);
+        }
+
+        // POST: Registration/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Firstname,Lastname,Nic,Course_Id,Batch_Id,Telno")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Batch_Id = new SelectList(db.Batches, "Id", "Batch1", registration.Batch_Id);
+            ViewBag.Course_Id = new SelectList(db.Courses, "Id", "Course1", registration.Course_Id);
+            return View(registration);
+        }
+
+        // GET: Registration/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: Registration/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Registration registration = db.Registrations.Find(id);
+            db.Registrations.Remove(registration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
